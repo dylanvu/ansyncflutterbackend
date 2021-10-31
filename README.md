8094570ec1bb935f8ee37caa0fe2fa88956e7bab
Dylan Vu <75911565+vu-dylan@users.noreply.github.com>
2021-10-30 23:14:26 -0700

    implement backend completely

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ansyncflutterbackend
+
+Associated dackend to the Water Level Flutter app for Ansync
+
+## Resources Used
+* Firestore Quickstart: https://firebase.google.com/docs/firestore/quickstart
+* Setting up Firestore on NodeJS: https://firebase.google.com/docs/admin/setup
+* Getting Data from Firestore: https://firebase.google.com/docs/firestore/query-data/get-data
---
+++ server.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import { createRequire } from 'module';
+import cors from 'cors';
+import { getFirestore } from 'firebase-admin/firestore';
+import admin from 'firebase-admin';
+
+const require = createRequire(import.meta.url);
+
+var serviceAccount = require('./ansyncflutter-firebase-adminsdk-h0uqf-140748fd92.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const db = getFirestore();
+
+const APP = express();
+APP.use(cors());
+APP.use(express.json());
+const PORT = 3000;
+
+APP.get('/', (req, res) => {
+    console.log("Get request for water level");
+    const waterLeveldoc = db.collection('water').doc('ansync');
+    getWaterlevel(waterLeveldoc, res);
+});
+
+APP.post('/', (req, res) => {
+    console.log('Got body: ', req.body);
+    const waterLeveldoc = db.collection('water').doc('ansync');
+    updateWaterlevel(req.body.newlevel, waterLeveldoc, res);
+});
+
+APP.listen(PORT, () => console.log(`Backend Application listening at http://localhost:${PORT}`));
+
+async function updateWaterlevel(newLevel, document, res) {
+    await document.set({
+        level: newLevel
+    });
+    res.sendStatus(201);
+}
+
+async function getWaterlevel(document, res) {
+    const waterDoc = await document.get();
+    if (!waterDoc.exists) {
+        console.log("Water Level does not exist");
+    } else {
+        // Send back the water level as a string
+        res.send(waterDoc.data().level.toString());
+    }
+}
